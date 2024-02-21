23b7be020791db6aeb9f3874a249f260bce87102
Samuel-Quenouille <quenouille_samuel@yahoo.com>
2024-02-21 12:18:49 +0100

    comments deleted

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import { prompt } from "./prompt.js";
 
-// Présentation du jeu
 console.log(
   `Bienvenue à GUESS THE NUMBER ! 🕹️ \nTon but est simple : deviner le nombre avec le moins d'essais possible ! \nLe nombre d'essais est illimité. \nBonne chance à toi !`
 );
 
-// On va générer un nombre aléatoire entre 0 et 100 qu'on va appeler targetNumber
 const min = 0;
 const max = 100;
 
@@ -16,7 +14,6 @@ const generateRandomNumber = (min, max) => {
 
 const targetNumber = generateRandomNumber(min, max);
 
-// Si le nombre n'est pas valide, il informe qu'il faut rentrer un nombre entre 0 et 100
 const validateNumberOfUser = (userNumber) => {
   if (Number.isNaN(userNumber) || Math.abs(userNumber) > 100) {
     console.log("🔴 Il faut rentrer un nombre entre 0 et 100");
@@ -25,7 +22,6 @@ const validateNumberOfUser = (userNumber) => {
   return true;
 };
 
-// On va demander à l'utilisateur de rentrer un nombre
 const promptNumber = (message) => {
   let userNumber = Number(prompt(message));
 
@@ -36,9 +32,6 @@ const promptNumber = (message) => {
   return userNumber;
 };
 
-// Si le nombre est plus petit, il va informer que le nombre targetNumber est plus grand
-// Si le nombre est trop grand, il va informer que le nombre targetNumber est plus petit
-// Si c'est le bon nombre : fin de la partie ! (affiche le nombre d'essais).
 const compareNumbers = (userNumber, targetNumber, attempts) => {
   if (userNumber < targetNumber) {
     console.log("Le nombre à trouver est plus grand 📈");
@@ -51,7 +44,7 @@ const compareNumbers = (userNumber, targetNumber, attempts) => {
   }
 
   userNumber = promptNumber("\nEntre ton nombre : ");
-  attempts++; // Il faut compter le nombre d'essais de l'utilisateur.
+  attempts++;
   compareNumbers(userNumber, targetNumber, attempts);
 };
 
